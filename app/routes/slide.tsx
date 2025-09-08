d2986cf51bb80bc12073e969322878065d12ea2d
laszloekovacs <laszloekovacs@gmail.com>
2025-09-08 13:27:11 +0200

    add image slideshow

--- app/routes/slide.tsx
+++ app/routes/slide.tsx
@@ -1,5 +1,6 @@
 import { readdir } from "node:fs/promises"
 import type { Route } from "./+types/slide"
+import { useEffect, useState } from "react"
 
 const path = "./public/slides"
 
@@ -23,12 +24,36 @@ export function HydrateFallback() {
     return <div>Loading...</div>;
 }
 
+const resolvePath = (filename: string) => {
+    return "./slides/" + filename
+}
+
 export default function Slide({ loaderData }: Route.ComponentProps) {
     const files = loaderData
+    const [index, setIndex] = useState(() => Math.floor(Math.random() * files.length))
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(prev => {
+                let next = prev + 1
+
+                // cap next
+                if (next >= files.length)
+                    next = 0
+
+                return next
+            })
+        }, 1000);
+
+        return () => clearInterval(interval)
+    }, [files])
+
+    const src = resolvePath(files[index])
 
     return (
         <div>
-            <p>slide</p>
+            <img src={src ?? null} alt={src ?? "missing file"} />
         </div>
     )
 }
