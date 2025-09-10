147b785314c12c04996003983a33d12e5c9f5dbf
laszloekovacs <laszloekovacs@gmail.com>
2025-09-10 10:54:11 +0200

    add cyrilic test string

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -3,7 +3,8 @@ export default function MainPage() {
 
     return (
         <div>
-            <p>main page</p>
+            <p>test string</p>
+            <p>локдаун</p>
         </div>
     )
 }
