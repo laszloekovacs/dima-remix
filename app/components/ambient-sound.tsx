5481dd77b88a2299b68bfb65afe4a9a807771174
laszloekovacs <laszloekovacs@gmail.com>
2025-10-30 13:31:54 +0100

    feat: add AmbientSound component for background audio

---
+++ app/components/ambient-sound.tsx
@@ -0,0 +1,4 @@
+/** biome-ignore-all lint/a11y/useMediaCaption: hidden audio file */
+export default function AmbientSound() {
+	return <audio src="./assets/computer-humming-236384.mp3" loop autoPlay />
+}
