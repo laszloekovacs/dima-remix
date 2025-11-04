9bb32324f770c755030d7b95ccce87d13571900d
laszlo kovacs <laszloekovacs@gmail.com>
2025-11-04 17:23:58 +0100

    removed ambient sounds

--- app/components/ambient-sound.tsx
+++
@@ -1,4 +0,0 @@
-/** biome-ignore-all lint/a11y/useMediaCaption: hidden audio file */
-export default function AmbientSound() {
-  return <audio src="./assets/computer-humming-236384.mp3" loop autoPlay />
-}
