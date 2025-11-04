9c7c47a1f4e7c13d6c782a267066dec55c58585b
laszloekovacs <laszloekovacs@gmail.com>
2025-11-04 11:58:14 +0100

    #16 created stripe css classes

--- app/routes/testing.tsx
+++ app/routes/testing.tsx
@@ -2,16 +2,8 @@ import SystemLayout from "app/components/system-layout"
 
 export default function TestingRoute(): React.ReactNode {
 	return (
-		<SystemLayout
-			main={
-				<div style={{ background: "#ff6b6b", borderRadius: 8, height: 12 }} />
-			}
-			heading={
-				<div style={{ background: "#ffd166", borderRadius: 8, height: 12 }} />
-			}
-			commandLine={
-				<div style={{ background: "#6bcB77", borderRadius: 8, height: 12 }} />
-			}
-		/>
+		<div>
+			<div className="warning-stripes background-animation">hello world</div>
+		</div>
 	)
 }
