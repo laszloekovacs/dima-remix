8412e331af7fab957e8115a67dfc46711ecb8ae1
laszloekovacs <laszloekovacs@gmail.com>
2025-11-05 16:57:52 +0100

    minor edits

--- app/routes/tapedetails.tsx
+++ app/routes/tapedetails.tsx
@@ -3,9 +3,17 @@ import SystemLayout from "~/components/system-layout"
 export default function TapeSelectionScreen() {
   return (
     <SystemLayout
-      heading={<p>dima</p>}
-      main={<div>hello</div>}
+      heading={<p>dima konzol</p>}
+      main={<DetailsScreen />}
       commandLine={<p>commands</p>}
     />
+  )
+}
+
+const DetailsScreen = () => {
+  return (
+    <div>
+      <p>Kiválasztott szalag: "Химера"</p>
+    </div>
   )
 }
