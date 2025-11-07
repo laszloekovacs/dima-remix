72c1e7ca2b435d927e8cc9c0a1d786e59cc980ec
laszlo kovacs <laszloekovacs@gmail.com>
2025-11-07 21:19:29 +0100

    add async exec #25

---
+++ app/utils/exec.server.ts
@@ -0,0 +1,7 @@
+import { promisify } from "node:util"
+import { exec, fork } from "node:child_process"
+
+// call exec for one shot calls, fork for long running tasks with updates
+
+export const asyncExec = promisify(exec)
+export const asyncFork = promisify(fork)
