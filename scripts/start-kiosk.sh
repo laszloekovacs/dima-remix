489445ad0a6a47a92b3748fdfdf3d4d3c7ede67b
laszloekovacs <laszloekovacs@gmail.com>
2025-11-13 17:38:49 +0100

    updated startup scripts

--- scripts/start-kiosk.sh
+++ scripts/start-kiosk.sh
@@ -19,7 +19,7 @@ SERVER_PID=$!
 
 # check with netcat if the socket is open
 # waits till the server is up
-# install sudo netcat
+# install netcat
 until nc -z -w 1 localhost:3000; do
     echo "Waiting for server start..."
     sleep 1
