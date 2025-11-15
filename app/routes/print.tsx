38cbb51563c21e94a8a14688c0908763abdd68bc
laszloekovacs <laszloekovacs@gmail.com>
2025-11-15 16:52:46 +0100

    removed old components

--- app/routes/print.tsx
+++
@@ -1,5 +0,0 @@
-// this should return to tape details when done
-
-export default function TapeSelectionScreen() {
-	return <p>printing</p>
-}
--- app/routes/tapeselect.tsx
+++
@@ -1,3 +0,0 @@
-export default function TapeSelectionScreen() {
-	return <p>tape selection screen</p>
-}
