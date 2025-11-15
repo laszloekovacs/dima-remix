794241ab1dcc164d58d9ebd70e02dc1c73ece666
laszloekovacs <laszloekovacs@gmail.com>
2025-11-15 16:51:14 +0100

    add timer to lockdown screen

--- app/routes/lockdown.tsx
+++ app/routes/lockdown.tsx
@@ -1,7 +1,23 @@
+import { useCallback } from "react"
+import { useNavigate } from "react-router"
+import { useCountdown } from "~/hooks/useCountdown"
+
 export default function LockdownPage() {
+  const navigate = useNavigate()
+
+  const handleTimeout = useCallback(() => {
+    navigate("/login")
+  }, [navigate])
+
+  const remaining = useCountdown({ seconds: 8 }, handleTimeout)
+
   return (
-    <div>
-      <h1>Rendszer Lezárva</h1>
+    <div className="min-h-screen min-w-screen grid place-content-center">
+      <h1>Внимание!</h1>
+      <h1>Rendszer Lezárva!</h1>
+
+      <p>Feloldásig hátralévő idő</p>
+      <p>{remaining.toLocaleString("HU")}</p>
     </div>
   )
 }
