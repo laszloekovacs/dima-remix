edf5c3e5cdf435c5110a9da1bd9cafc5e618abfe
laszlo kovacs <laszloekovacs@gmail.com>
2025-11-18 18:54:05 +0100

    add carousel page

---
+++ app/routes/carousel.tsx
@@ -0,0 +1,57 @@
+import { readdir } from "node:fs/promises"
+import useSlideshow from "~/hooks/useSlideshow"
+import type { Route } from "./+types/carousel"
+import { formatStopwatch, useCountdown } from "~/hooks/useCountdown"
+import { useNavigate } from "react-router"
+
+const SLIDES_PATH = "/public/slides"
+
+export const loader = async () => {
+  // get image filenames from disk
+  const slides = await readdir(process.cwd() + SLIDES_PATH)
+
+  // read duration from kvstore or set default
+  const duration = 800
+
+  return {
+    duration,
+    slides,
+  }
+}
+
+export default function Carousel({ loaderData }: Route.ComponentProps) {
+  const { duration, slides } = loaderData
+
+  const navigate = useNavigate()
+
+  const handleCountdownEnd = () => {
+    navigate("/tape")
+  }
+
+  const image = useSlideshow(slides, duration)
+  const countdown = useCountdown({ seconds: 5 }, () => handleCountdownEnd())
+
+  return (
+    <div className="h-screen w-screen p-16 overflow-hidden">
+      <div
+        className="w-full h-full grid place-content-center"
+        style={{
+          backgroundImage: `url(/slides/${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="bg-black text-amber-500 p-6 border-8 border-black leading-normal overflow-hidden">
+          <p>Adatszalag betoltese</p>
+          <p className="text-center bg-red-500 text-black text-9xl blink-slow">
+            {formatStopwatch(countdown).asString}
+          </p>
+          <div className="h-5">
+            <div className="background-animation warning-stripes"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+//<img src={`/slides/${image}`} alt={image} className="overflow-hidden" />
